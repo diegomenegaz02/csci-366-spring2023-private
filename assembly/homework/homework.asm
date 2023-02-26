7f5b62ad5d283677d94aad32b6417ea37a699203
diegomenegaz02 <99154053+diegomenegaz02@users.noreply.github.com>
2023-02-25 17:10:27 -0800

    Update homework.asm
    Added Problem 1

--- assembly/homework/homework.asm
+++ assembly/homework/homework.asm
@@ -1,7 +1,17 @@
 //==========================================================================================
 // PROBLEM 1: Counting Up From 1 to 10 in Little Man Assembly
 //==========================================================================================
-
+START   LDA COUNT
+        OUT
+        ADD ONE
+        STA COUNT
+        LDA LOOPS
+        SUB COUNT
+        BRP START
+        HLT
+COUNT   DAT 1
+ONE     DAT 1
+LOOPS   DAT 10
 // You can develop your code via the online emulator and then paste it in here
 // Please use a jump in your solution
 // Please use the OUT instruction to print values
