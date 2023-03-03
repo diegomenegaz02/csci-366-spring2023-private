d4648df3f217cac54cbfd3396e4120035b3cfa24
diegomenegaz02 <99154053+diegomenegaz02@users.noreply.github.com>
2023-03-03 09:59:26 -0800

    Update homework.asm

--- assembly/homework/homework.asm
+++ assembly/homework/homework.asm
@@ -19,6 +19,26 @@ LOOPS   DAT 10
 //==========================================================================================
 // PROBLEM 2: Counting Up From 1 to 10 in MIPS Assembly
 //==========================================================================================
+.data
+
+.text
+	addi $8, $0, 0
+	addi $9, $0, 10
+	startloop:
+	bgt $8, $9, exit
+	li $v0 , 1
+	move $a0, $8
+	syscall
+	
+	addi $8, $8 ,1
+	j startloop
+	
+	exit:
+	li $v0, 10
+	syscall
+	
+	
+
 
 // You can develop your code via the MARS emulator and then paste it in here
 // Please use a jump in your solution
