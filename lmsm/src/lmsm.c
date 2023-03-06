c2358c347479ab81b09b25ca27ffc172efeb7121
diegomenegaz02 <99154053+diegomenegaz02@users.noreply.github.com>
2023-03-05 17:43:10 -0800

    Test1

--- lmsm/src/lmsm.c
+++ lmsm/src/lmsm.c
@@ -30,6 +30,10 @@ void lmsm_i_ret(lmsm *our_little_machine) {
 }
 
 void lmsm_i_push(lmsm *our_little_machine) {
+    //want to push value of acc onto the value stack( starting at 199)
+    our_little_machine-> stack_pointer --;
+    our_little_machine->memory[our_little_machine->stack_pointer] = our_little_machine->accumulator;
+
 }
 
 void lmsm_i_pop(lmsm *our_little_machine) {
@@ -74,6 +78,8 @@ void lmsm_i_load(lmsm *our_little_machine, int location) {
 }
 
 void lmsm_i_add(lmsm *our_little_machine, int location) {
+    //returning last 2 digits because between 100-199
+    our_little_machine->accumulator +=our_little_machine->memory[location];
 }
 
 void lmsm_i_sub(lmsm *our_little_machine, int location) {
@@ -123,6 +129,8 @@ void lmsm_exec_instruction(lmsm *our_little_machine, int instruction) {
         lmsm_i_halt(our_little_machine);
     } else if (100 <= instruction && instruction <= 199) {
         lmsm_i_add(our_little_machine, instruction - 100);
+    } else if (instruction == 920) {
+        lmsm_i_push(our_little_machine);
     } else {
         our_little_machine->error_code = ERROR_UNKNOWN_INSTRUCTION;
         our_little_machine->status = STATUS_HALTED;
